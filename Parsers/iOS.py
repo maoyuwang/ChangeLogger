75057156a50185821835ca6eca7363713d1da089
maoyuwang <github@maoyu.wang>
2019-09-19 19:48:26 -0400

    Approach iOS.py

--- Parsers/iOS.py
+++ Parsers/iOS.py
@@ -14,7 +14,18 @@ class iOS(Parser):
         print(versions)
 
         DIVs = DIV.find_all("div", {"id": re.compile("^\d{1,}$")})
-        print(DIVs[4].text)
+        for i in range(0,len(DIVs)):
+            content = DIVs[i].div
+            uls = content.ul
+            if uls!=None:
+                uls = content.find_all("ul")
+                for ul in uls:
+                    print(ul.text)
+            else:
+                print(content.text)
+            # step2 = update_content.find_all("ul").find_all("li")
+            # print(update_content)
+            # print(DIVs[i].text)
 
 
 if __name__ == '__main__':
