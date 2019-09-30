00137e3fc4b2992309ae4d7f4183fbe54ea20fe2
maoyuwang <github@maoyu.wang>
2019-09-30 16:31:27 -0400

    Setup cron module

---
+++ Parsers/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["Curl","iOS","Nginx","OpenJDK","TelegramMac","TelegramDesktop","VisualStudioCode"]
---
+++ cron.py
@@ -0,0 +1,10 @@
+from Parsers import *
+
+parserList = [Curl.Curl(),
+              iOS.iOS(),
+              Nginx.Nginx(),
+              OpenJDK.OpenJDK(),
+              TelegramDesktop.TelegramDesktop(),
+              TelegramMac.TelegramMac(),
+              VisualStudioCode.VisualStudioCode()]
+
