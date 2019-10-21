422517c014bfbdd0cd0f89a9e26206a12219ff81
syhdd <syhdd2016@gmail.com>
2019-10-20 23:24:45 -0400

    test on ignore

--- db.php
+++ db.php
@@ -3,9 +3,9 @@ include 'config.php';
 class db
 {
     var $conn;
-    function __construct($servername, $username,$password,$dbname)
+    function __construct($servername, $username,$password,$dbname,$port)
     {
-        $this->conn = new mysqli($servername, $username, $password,$dbname);
+        $this->conn = new mysqli($servername, $username,$password,$dbname,$port);
         if($this->conn) {
             echo "Connection established\n";
         }else{
@@ -13,27 +13,35 @@ class db
             die( print_r( sqlsrv_errors(), true));
         }
     }
-    function translate(){
-        $query="SELECT * FROM 'data'";
-        $result = $this->conn->query($query);
-        if(is_null($result)){
-            echo "Sccuess";
-        }else{
-            echo "Fail";
+    //translating sql sentence to array(if is select)
+    function sql($query,$args,$isselect){
+        $ret=null;
+        $where="1";
+        if(!is_null($args)){
+            $where=$args;
         }
-        /*if ($result->num_rows > 0) {
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-                echo "id: " . $row["ID"]. " - Name: " . $row["Name"]. " " . $row["Description"]. "<br>";
+        $query=$query ." WHERE ". $where;
+        $result = $this->conn->query($query);
+        if($isselect){
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    $set[] = $row;
+                    $ret=$set;
+                    echo "\n";
+                }
+            } else {
+                echo "0 results";
             }
-        } else {
-            echo "0 results";
-        }*/
+        }
+        return $ret;
     }
     function  close(){
         CloseCon($this->conn);
     }
 }
-$test=new db($servername, $username,$password, $dbname);
-$test->translate();
+$test=new db($servername,$username,$password, $dbname,$port);
+$query="Select * from softwares";
+$args=Null;
+print_r($test->sql($query,$args,true));
 ?>
