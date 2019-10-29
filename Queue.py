aa3f4cc63707f63e12480b087b3e66430fd92c73
maoyuwang <github@maoyu.wang>
2019-10-29 13:51:48 -0400

    Finish Notification Send

--- Queue.py
+++ Queue.py
@@ -1,17 +1,42 @@
-from Redis import redis
+import redis
+from config import *
 
 class Queue(object):
-    def __init__(self,queueName):
-        self.queueName = queueName
-        self.db = redis()
+    """Simple Queue with Redis Backend"""
+    def __init__(self, name, namespace='queue'):
+        """The default connection parameters are: host='localhost', port=6379, db=0"""
+        self.__db= redis.Redis(host=REDIS_HOST,port=REDIS_PORT,db=REDIS_DB,password=REDIS_PASSWORD)
+        self.key = '%s:%s' %(namespace, name)
+
+    def qsize(self):
+        """Return the approximate size of the queue."""
+        return self.__db.llen(self.key)
 
     def __len__(self):
-        return self.db.llen(self.queueName)
+        return self.qsize()
 
-    def push(self,value):
-        return self.db.rpushx(self.queueName,value)
+    def empty(self):
+        """Return True if the queue is empty, False otherwise."""
+        return self.qsize() == 0
 
-    def pop(self):
-        return self.db.lpop(self.queueName)
+    def put(self, item):
+        """Put item into the queue."""
+        self.__db.rpush(self.key, item)
 
-    
+    def get(self, block=True, timeout=None):
+        """Remove and return an item from the queue.
+
+        If optional args block is true and timeout is None (the default), block
+        if necessary until an item is available."""
+        if block:
+            item = self.__db.blpop(self.key, timeout=timeout)
+        else:
+            item = self.__db.lpop(self.key)
+
+        if item:
+            item = item[1]
+        return item
+
+    def get_nowait(self):
+        """Equivalent to get(False)."""
+        return self.get(False)
--- cron-notifications.py
+++ cron-notifications.py
@@ -1,18 +1,25 @@
 from Queue import Queue
 from Notifications import SMS
 from Notifications import Email
+import json
 def emailQueue():
     q = Queue("email")
+    print(len(q))
     while(len(q)!=0):
-        (address,subject,content) = q.pop()
-        # Email.send(address,subject,content)
+        msg = json.loads(q.get())
+        address = msg['address']
+        subject = msg['subject']
+        content = msg['content']
+        Email.send(address,subject,content)
         print((address,subject,content))
 
 def smsQueue():
     q = Queue("sms")
     while (len(q) != 0):
-        (address, content) = q.pop()
-        # SMS.send(address,content)
+        msg = json.loads(q.get())
+        address = msg['address']
+        content = msg['content']
+        SMS.send(address,content)
         print((address,content))
 
 if __name__ == '__main__':
