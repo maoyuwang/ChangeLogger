380287140dd5f54caa064524193b4cd164f3c90c
syhdd <syhdd2016@gmail.com>
2019-11-03 19:42:01 -0500

    test3

--- Website/feed.php
+++ Website/feed.php
@@ -4,16 +4,13 @@ include_once "SoftwareController.php";
 $software = new Software($softwareID);
 $changelogs = $software->getChangelogs();
 $latestVersion = $changelogs[0]['Version'];
-
 $xml = new SimpleXMLElement('<xml/>');
-
 for ($i = 1; $i <= 8; ++$i) {
     $track = $xml->addChild('item');
     $track->addChild('update_time', "1999/99");
     $track->addChild('version', "0.0");
     $track->addChild('content', "nothing");
 }
-
 Header('Content-type: text/xml');
 print($xml->asXML());
 ?>
