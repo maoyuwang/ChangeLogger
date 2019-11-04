33a890f4454cdd64be4d48cfb1d885af8ec10654
syhdd <syhdd2016@gmail.com>
2019-11-03 21:07:10 -0500

    new_test 7

--- Website/feed.php
+++ Website/feed.php
@@ -10,16 +10,21 @@ $softwareID = $_GET["softwareID"];
 $software = new Software($softwareID);
 $changelogs = $software->getChangelogs();
 $length = count($changelogs);
+$name=$software->getName();
 
 echo "<title>Demo RSS Feed</title>\n";
 echo "<description>RSS Description</description>\n";
 //echo "<link>http://www.mydomain.com</link>\n";
 
 for($i = 0; $i < $length;$i++){
-    echo "<item>n";
-    echo "<version>$changelogs[$i]['Version']</version>\n";
-    echo "<time>$changelogs[$i]['Time']</time>\n";
-    echo "<details>$changelogs[$i]['Detail']</details>\n";
+    $version=$changelogs[$i]['Version'];
+    $time=$changelogs[$i]['Time'];
+    $detail=$changelogs[$i]['Detail'];
+
+    echo "<item>$name";
+    echo "<version>$version</version>\n";
+    echo "<time>$time</time>\n";
+    echo "<details>$detail</details>\n";
     echo "<atom:link href='https://changelogger.org/feed.php?$softwareID' rel='self' type='application/rss+xml'/>\n";
     echo "</item>\n";
 
