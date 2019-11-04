cad6a7aa31312d3de7eb6ee59445f43162b72009
maoyuwang <github@maoyu.wang>
2019-11-04 15:56:16 -0500

    Prepare for interim release

--- Parsers/testSoftware.py
+++ Parsers/testSoftware.py
@@ -6,13 +6,20 @@ from Parsers.Parser import *
 class testSoftware(Parser):
     def parse(self):
         parseResult = []
-        record = dict()
-        record['version'] = "1.0.0"
-        record['time'] = "2019-10-29"
-        record['content'] = ["11111",'22222','33333']
+        record1 = dict()
+        record1['version'] = "1.0.0"
+        record1['time'] = "2019-10-29"
+        record1['content'] = ["11111",'22222','33333']
 
-        # 将本次更新添加至最终解析结果中去
-        parseResult.append(record)
+        record2 = dict()
+        record2['version'] = "1.0.1"
+        record2['time'] = "2019-11-02"
+        record2['content'] = ["aaaa",'bbbbb','cccc']
+
+        # Append update info to result list.
+
+        parseResult.append(record1)
+        # parseResult.append(record2)
 
         # 返回最终解析结果
         return (getJSONStr(parseResult))
