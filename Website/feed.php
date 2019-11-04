d8e8ac0dba4f094ae7f3b9d0022b401ce0562b6c
syhdd <syhdd2016@gmail.com>
2019-11-03 20:02:49 -0500

    test10

--- Website/feed.php
+++ Website/feed.php
@@ -2,7 +2,5 @@
 include_once "SoftwareController.php";
 $softwareID = $_GET["softwareID"];
 $software = new Software($softwareID);
-$changelogs = $software->getChangelogs();
-$length = count($changelogs);
 echo $softwareID;
 ?>
